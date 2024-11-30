658c3da168e7ee8c49ee8acad028233e72958bc1
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-11-30 15:12:34 +0200

    File

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -51,7 +51,7 @@ const MobileNav = () => {
           <div className="mt-32 mb-40 text-center text-2xl">
             <Link href="/">
               <h1 className="text-4xl font-semibold">
-                Hossam<span className="text-accent">.</span>
+                Saeed<span className="text-accent">.</span>
               </h1>
             </Link>
           </div>
